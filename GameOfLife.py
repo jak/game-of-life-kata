a9cf516e74573a74f6ecee9345ea9b5d25ca4ce6
Stefanos <stefanos.platonis@moo.com>
2015-02-03 16:46:41 +0000

    Code kata file initial state

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,4 +1,61 @@
 __author__ = 'stefanos'
 
-def test_game_of_life():
-    pass
+
+class GameOfLife:
+    """
+    Game of Life class
+    """
+
+    def __init__(self, seed):
+        pass
+
+    def next(self):
+        return set()
+
+
+def test_blinker_pattern():
+    """
+    .....
+    .***.
+    .....
+
+    =>
+
+    ..*..
+    ..*..
+    ..*..
+    """
+
+    seed = {(1, 1), (2, 1), (3, 1)}
+    game = GameOfLife(seed)
+    assert game.next() == {(2, 0), (2, 1), (2, 2)}
+    assert game.next() == seed
+
+
+def test_beacon_pattern():
+    """
+    **..
+    **..
+    ..**
+    ..**
+
+    =>
+
+    **..
+    *...
+    ...*
+    ..**
+
+    =>
+
+    **..
+    **..
+    ..**
+    ..**
+
+    """
+
+    seed = {(0, 0), (1, 0), (0, 1), (1, 1), (2, 2), (3, 2), (2, 3), (3, 3)}
+    game = GameOfLife(seed)
+    assert game.next() == {(0, 0), (1, 0), (0, 1), (3, 2), (2, 3), (3, 3)}
+    assert game.next() == seed
