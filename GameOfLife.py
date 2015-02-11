f9fcb90bab315393e35df4ed4dffe343b165ef6a
Jak Spalding <jak@moo.com>
2015-02-11 09:33:40 +0000

    block pattern

--- GameOfLife.py
+++ GameOfLife.py
@@ -6,14 +6,60 @@ class GameOfLife:
     Game of Life class
     """
 
+    state = set()
+
     def __init__(self, seed):
-        pass
+      self.state = seed
 
     def next(self):
-        return set()
+        new_state = set()
+        for cell in self.state:
+          if self.should_stay_alive(cell):
+            new_state.add(cell)
 
+        self.state = new_state
+        return new_state
 
-def test_blinker_pattern():
+    def c2p(self, cell):
+      return cell.point()
+
+    def should_stay_alive(self, cell):
+      alive_neighbours = self.neighbours(cell) & self.state
+      print alive_neighbours
+      return len(alive_neighbours) in [2,3]
+
+    def is_alive(self, cell):
+      return cell in self.state
+
+    def neighbours(self, cell):
+      cells = set()
+      for x in range(-1,2):
+        for y in range(-1,2):
+          if x != 0 or y != 0:
+            cells.add((cell[0] + x, cell[1] + y))
+      return cells
+
+def test_single_cell_dies():
+  seed = set([(0, 0)])
+  game = GameOfLife(seed)
+  assert game.next() == set()
+
+def test_one_neighbour_dies():
+  seed = set([(0,0), (1,0)])
+  game = GameOfLife(seed)
+  assert game.next() == set()
+
+def test_block_pattern():
+  """
+  **-
+  **-
+  ---
+  """
+  seed = set([(0,0), (1,0), (0, 1), (1,1)])
+  game = GameOfLife(seed)
+  assert game.next() == seed
+
+def hid_test_blinker_pattern():
     """
     .....
     .***.
@@ -32,7 +78,7 @@ def test_blinker_pattern():
     assert game.next() == seed
 
 
-def test_beacon_pattern():
+def hid_test_beacon_pattern():
     """
     **..
     **..
